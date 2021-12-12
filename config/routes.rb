e5c081b7613d324d60c1559eedddae3853999a2c
sho135 <sh135pro@gmail.com>
2021-12-12 18:11:48 +0000

    Edit:top

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
- root 'homes#top'
+ root to: 'homes#top'
 
   resources :posts do
     resources :post_comments, only: [:create, :destroy]
-    resource :likes, only: [:create, :destroy]
+    resources :likes, only: [:create, :destroy]
   end
+
+   resources :users, only: [:show]
 
 end
