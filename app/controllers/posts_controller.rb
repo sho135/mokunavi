b25c9f7de1d16621c58a71f70797c93d7bdd7610
sho135 <sh135pro@gmail.com>
2021-12-17 00:40:34 +0000

    Edit:views index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,31 @@
 class PostsController < ApplicationController
-  
+
   def new
   end
-  
+
   def create
   end
-  
+
   def index
+    @user = current_user
+    @posts = Post.all
   end
-  
+
   def show
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:user_id, :title, :body, :image_id)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def index
+    @user = current_user
+    @post = Posts.all
+
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :text, :image_id)
+  end
+
 end
