0612524b40f71d48d42d4f1704fa9199ef085576
sho135 <sh135pro@gmail.com>
2021-12-17 04:39:07 +0000

    Edit:views user edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
 
   has_many :post_comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  attachment :image
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'homes#top'
 
 
   resources :posts do
@@ -11,7 +12,5 @@ Rails.application.routes.draw do
   end
 
   resources :users
-
-  root to: 'homes#top'
 
 end
