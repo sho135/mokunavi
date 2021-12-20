2222537b71b3a4b2a305df894f9615ba7fe9fe4d
sho135 <sh135pro@gmail.com>
2021-12-20 13:01:01 +0000

    Edit:views posts users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,9 +26,11 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
   end
 
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def md_to_html(text)
+    options = {
+      filter_html:     true,
+      hard_wrap:       true,
+      space_after_headers: true,
+    }
+
+    extensions = {
+      autolink:           true,
+      no_intra_emphasis:  true,
+      fenced_code_blocks: true,
+    }
+
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+    markdown.render(text).html_safe
+  end
 end
