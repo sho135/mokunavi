ba9f4e1d702d28bcff5415881aa0d4021be1e76b
sho135 <sh135pro@gmail.com>
2021-12-24 12:21:10 +0000

    Edit:controllers views users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @post = Post.new
-    @posts = @user.posts
+    @posts = @user.posts.page(params[:page]).reverse_order
+
   end
 
   def edit
